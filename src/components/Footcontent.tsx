a9517319c367e8557cd4f29f7c4b2097b22a105d
Claudia Nohlgård <claudia@nohlgard.se>
2024-11-05 12:47:55 +0100

    Footer colors, feature etc.

--- src/components/Footcontent.tsx
+++ src/components/Footcontent.tsx
@@ -8,19 +8,20 @@ type IconLink = {
 };
 
 const iconLinks: IconLink[] = [
-    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/claudianohlgard/', icon: <LinkedinLogo size={24} /> },
-    { label: 'GitHub', href: 'https://github.com/geekmorsan', icon: <GithubLogo size={24} /> },
-    { label: 'Behance', href: 'https://www.behance.net/nohlgard', icon: <BehanceLogo size={24} /> },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/claudianohlgard/', icon: <LinkedinLogo size={28} color="currentColor" /> },
+    { label: 'GitHub', href: 'https://github.com/geekmorsan', icon: <GithubLogo size={28} color="currentColor" /> },
+    { label: 'Behance', href: 'https://www.behance.net/nohlgard', icon: <BehanceLogo size={28} color="currentColor" /> },
 ];
 
 const Footcontent: React.FC = () => (
-    <div className="flex items-center justify-around p-4">
-        <div className="flex gap-4">
+    <div className="flex items-center border-t w-[40vw] border-accent-1-hover justify-between flex-col gap-2 md:gap-4 p-4 md:p-6">
+        <div className="flex md:mt-2 gap-4">
             {iconLinks.map(({ href, icon, label }) => (
                 <a
                     key={href}
                     href={href}
-                    className="p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline focus:outline-2 focus:outline-blue-500"
+                    className="p-2 rounded hover:outline-accent-1-hover text-accent-1-dark hover:text-accent-1-light
+                    transform scale-100 transition-transform duration-200 hover:scale-125"
                     target="_blank"
                     rel="noopener noreferrer"
                     aria-label={label}
@@ -29,7 +30,7 @@ const Footcontent: React.FC = () => (
                 </a>
             ))}
         </div>
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm text-type-2 text-center">
             © 2024 Claudia Nohlgård. All Rights Reserved.
         </span>
     </div>
